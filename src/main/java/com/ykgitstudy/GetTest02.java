c2425b459c6a50ce673bcbf312a81817a1f0d694
YuKai <18030658127@163.com>
2022-08-22 19:06:23 +0800

    第一次提交（Vagrant）

---
+++ src/main/java/com/ykgitstudy/GetTest02.java
@@ -0,0 +1,11 @@
+package com.ykgitstudy;
+
+/**
+ * @author yk
+ * @date 2022/8/22 19:00
+ */
+public class GetTest02 {
+    public static void main(String[] args) {
+        System.out.println("========GetTest02 test 01========");
+    }
+}
---
+++ src/main/java/com/ykgitstudy/GitTest01.java
@@ -0,0 +1,11 @@
+package com.ykgitstudy;
+
+/**
+ * @author yk
+ * @date 2022/8/22 18:48
+ */
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("==========GitTest01 test01==========");
+    }
+}
