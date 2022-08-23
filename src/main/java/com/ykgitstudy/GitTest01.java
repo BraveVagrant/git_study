c2a7731c5ab11bad3db2ca495c133069b46967a3
YuKai <18030658127@163.com>
2022-08-23 10:32:42 +0800

    第一次提交（Vagrant）by hot_fix

--- src/main/java/com/ykgitstudy/GitTest01.java
+++ src/main/java/com/ykgitstudy/GitTest01.java
@@ -9,5 +9,6 @@ public class GitTest01 {
         System.out.println("==========GitTest01 test01==========");
         System.out.println("==========GitTest01 test02==========");
         System.out.println("==========GitTest01 test03==========");
+        System.out.println("==========GitTest01 test01 by hot_fix==========");
     }
 }
